cc2ba06a529552a4ee307e017220bd9257498f61
Felix Waigner <Waigner.Felix@t-online.de>
2024-01-27 02:25:35 +0100

    Moved to react-flow version 12. Migrated the inputNode and topicListNode to standalone nodes. Added the data-pass and addNode functions for inputNode and topicListNode

--- src/components/nodes/inputNode.tsx
+++ src/components/nodes/inputNode.tsx
@@ -1,124 +1,49 @@
-import { Idea } from '@/shared/types/idea.type';
+import { memo, useState, useCallback } from "react";
+import { NodeProps, useReactFlow } from "@xyflow/react";
 
-import React, { useEffect, useState } from 'react';
-
-import { HuggingFaceInference } from "@langchain/community/llms/hf";
-
-
-import { StructuredOutputParser } from "langchain/output_parsers";
-import { RunnableSequence } from "@langchain/core/runnables";
-import { PromptTemplate } from "@langchain/core/prompts";
-import { OpenAI } from "@langchain/openai";
-import { z } from "zod";
-
-import { Input } from "@/components/ui/input"
 import {
     Card,
-    CardContent
+    CardContent,
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
-import TopicListNode from '@/components/nodes/topicListNode';
+function InputNode() {
+
+    var reactFlow = useReactFlow();
+
+    const [inputText, setInputText] = useState("");
 
 
-export default function inputNode() {
 
-    const [inputValue, setInputValue] = useState('');
-    const [ideas, setIdeas] = useState<Idea[]>([]);
-
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value);
-    };
-
-    const generateText = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        // const parser = StructuredOutputParser.fromZodSchema(
-        //     z.array(
-        //         z.object({
-        //             fields: z.object({
-        //                 Title: z
-        //                     .string()
-        //                     .describe("Title of the topic")
-        //             })
-        //         })
-        //     ).describe("An array of Titles, each representing a reagrding topic to the question ")
-        // );
-
-        // const chain = RunnableSequence.from([
-        //     PromptTemplate.fromTemplate(
-        //         `break down the topic Generative ai influencers. 
-        //         Give me 5 Headlines without further description. 
-        //         Use maximum 4 Words.
-        //        \n{format_instructions}\n{question}`
-        //     ),
-        //     new OpenAI({
-        //         openAIApiKey: import.meta.env.VITE_OPENAI,
-        //         modelName: "gpt-4",
-        //         temperature: 0,
-        //     }),
-        //     parser,
-        // ]);
-        // const response = await chain.invoke({
-        //     question: inputValue,
-        //     format_instructions: parser.getFormatInstructions(),
-        // });
-        // setIdeas(response);
-        setIdeas([
+    const createTopicListNode = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        reactFlow.addNodes([
             {
-                "fields": {
-                    "Title": "AI-Driven Ad Creation"
-                }
-            },
-            {
-                "fields": {
-                    "Title": "Generative AI in Marketing"
-                }
-            },
-            {
-                "fields": {
-                    "Title": "AI and Creative Advertising"
-                }
-            },
-            {
-                "fields": {
-                    "Title": "Automated Ad Design"
-                }
-            },
-            {
-                "fields": {
-                    "Title": "AI in Ad Innovation"
-                }
+                id: (reactFlow.getNodes().length + 1).toString(),
+                data: { text: inputText },
+                type: 'topicListNode',
+                position: { x: 100, y: 100 },
             }
-        ])
-    }
-
-
-    useEffect(() => {
-        console.log(ideas);
-    }, [ideas]);
-
-
+        ]);
+    }, [inputText]);
 
     return (
         <div className="input-Node">
-            {ideas.length === 0 ? (
-                <Card className='p-2'>
-                    <CardContent className='p-0'>
-                        <form action="submit" className='flex gap-1' onSubmit={generateText}>
-                            <Input id="text" name="text" onChange={handleInputChange} placeholder='Enter a topic you want to explore...' className="nodrag w-[360px]" />
-                            <Button className='shadow-none p-2' type='submit'>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                                </svg>
-                            </Button>
-                        </form>
-                    </CardContent>
-                </Card>
-            ) : (
-                <TopicListNode ideas={ideas} />
-            )}
+            <Card className='p-2'>
+                <CardContent className='p-0'>
+                    <form action="submit" onSubmit={createTopicListNode} className='flex gap-1' >
+                        <Input id="text" name="input" onChange={(e) => setInputText(e.target.value)} placeholder='Enter a topic you want to explore...' className="nodrag w-[360px]" />
+                        <Button className='shadow-none p-2' type='submit'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                            </svg>
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 }
+
+export default memo(InputNode);
--- src/components/nodes/topicListNode.tsx
+++ src/components/nodes/topicListNode.tsx
@@ -1,6 +1,6 @@
 import { Idea } from "@/shared/types/idea.type";
 
-import React, { useCallback, useState, useRef } from "react";
+import { memo } from "react";
 
 import {
     Card,
@@ -10,27 +10,50 @@ import {
     CardTitle
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-
 import addTopicSvg from '@/images/svgs/addTopic.svg';
+import { NodeProps } from "@xyflow/react";
 
-export default function TobicListNode({ ideas }: { ideas: Idea[] }) {
-
-
-
-    const click = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(e.currentTarget.value)
+var ideas = [
+    {
+        "fields": {
+            "Title": "AI-Driven Ad Creation"
+        }
+    },
+    {
+        "fields": {
+            "Title": "Generative AI in Marketing"
+        }
+    },
+    {
+        "fields": {
+            "Title": "AI and Creative Advertising"
+        }
+    },
+    {
+        "fields": {
+            "Title": "Automated Ad Design"
+        }
+    },
+    {
+        "fields": {
+            "Title": "AI in Ad Innovation"
+        }
     }
+]
+
+function TopicListNode({ id, data }: NodeProps) {
 
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Topics</CardTitle>
-                <CardDescription>Learn more about the individual topics</CardDescription>
+                <CardTitle>{data.text}</CardTitle>
+                <CardDescription>{id}</CardDescription>
+                {/* <CardDescription>Learn more about the individual topics</CardDescription> */}
             </CardHeader>
             <CardContent>
                 <div className='flex flex-col gap-1 items-start '>
                     {ideas.map((idea, index) => (
-                        <Button variant={'outline'} className='justify-between w-full px-0 py-0 mt-1' key={index} value={idea.fields.Title} onClick={click}>
+                        <Button variant={'outline'} className='justify-between w-full px-0 py-0 mt-1' key={index} value={idea.fields.Title}>
                             <span className="px-4 py-2">{String(idea.fields.Title)}</span>
                             <div className="h-full px-2 py-2 ml-2 bg-gray-100 rounded-r-md">
                                 <img className="w-4 h-4 " src={addTopicSvg} />
@@ -43,3 +66,5 @@ export default function TobicListNode({ ideas }: { ideas: Idea[] }) {
         </Card>
     )
 }
+
+export default memo(TopicListNode);
